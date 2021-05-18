68888c846d8cf6b80f79a968509854095ccf1b69
konrad <ktalaga90@gmail.com>
2021-05-18 16:12:09 +0100

    BusStop class added

---
+++ src/main/java/BusStop.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class BusStop {
+
+    private String name;
+    private ArrayList<Person> queue;
+
+    public BusStop(){
+        this.name = name;
+        this.queue = new ArrayList<Person>();
+    }
+
+}
